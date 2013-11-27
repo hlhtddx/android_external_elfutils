69902cf11fa1ac4f22b1ade561acd3f780080803
Ben Cheng <bccheng@google.com>
2013-11-26 16:25:30 -0800

    [Work In Progress] Cleanup patches needed by bionic.
    Change-Id: Ie1800c782657e6bdfbc818d711fbd8f066c5aaf6

--- 0.153/bionic-fixup/AndroidFixup.h
+++ 0.153/bionic-fixup/AndroidFixup.h
@@ -1,24 +1,4 @@
-/*
- * Copyright 2013, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef ANDROID_FIXUP_H
-#define ANDROID_FIXUP_H
-
-#include <stdio.h>
-#include <libgen.h>     // for basename
+#include <libgen.h> // for basename
 
 #define off_t           loff_t
 
@@ -30,18 +10,16 @@
 #define MIN(x,y)        ((x) < (y) ? (x) : (y))
 #endif
 
-#ifndef powerof2
-#define powerof2(x)     (((x - 1) & (x)) == 0)
-#endif
-
-/* workaround for canonicalize_file_name */
-#define canonicalize_file_name(path) realpath(path, NULL)
-
-/* workaround for open64 */
-#define open64(path, flags)     open(path, ((flags) | O_LARGEFILE))
-
-/* no internalization */
-#define gettext(x)      (x)
+/* workaround for stpcpy */
+static inline char *stpcpy(char *dst, const char *src)
+{
+    while (*src) {
+        *dst++ = *src++;
+    }
+    // Need to copy the NULL terminator
+    *dst = *src;
+    return dst;
+}
 
 /* _mempcpy and mempcpy */
 #ifndef __mempcpy
@@ -68,16 +46,8 @@ static inline void *rawmemchr(const void *s, int c)
     }
 }
 
-/* workaround for stpcpy */
-static inline char *stpcpy(char *dst, const char *src)
-{
-    while (*src) {
-        *dst++ = *src++;
-    }
-    return dst;
-}
+/* workaround for canonicalize_file_name */
+#define canonicalize_file_name(path) realpath(path, NULL)
 
-/* forward declarations */
-char * dgettext (const char * domainname, const char * msgid);
-
-#endif /* ANDROID_FIXUP_H */
+/* workaround for open64 */
+#define open64(path, flags)     open(path, ((flags) | O_LARGEFILE))
